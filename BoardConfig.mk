777f9844fee3ac5a7eaaae2bbbfad78408ad78b4
Michael Benedict <michaelbt@live.com>
2018-07-21 22:10:06 -0700

    starqlte: initial commit
    Signed-off-by: Michael Benedict <michaelbt@live.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+DEVICE_PATH := device/samsung/starqlte
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := starqltechn,starqltezh,starqlte
+
+# Kernel
+TARGET_KERNEL_CONFIG := starqlte_chn_open_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Inherit common board flags
+include device/samsung/sdm845-common/BoardConfigCommon.mk
